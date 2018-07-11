f267e759f16651b51ee553dba508aa2847373d4c
Nguyen Thanh Long <longnt189@gmail.com>
2018-07-11 19:29:57 +0700

    fix

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
-app.use('/public', express.static('./public'));
+app.use('/public', express.static('public'));
 app.use(morgan('dev'));
 app.use(mainRouter);
 
